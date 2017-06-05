358a65d208632990ff29a6174970d4195a60ecfc
Makoto Inoue <inouemak@googlemail.com>
2017-06-06 01:03:20 +0900

    Refactor browse function

--- bots/welcome.js
+++ bots/welcome.js
@@ -2,24 +2,30 @@ status.addListener("init", function () {
   return {"text-message": "Welcome to Genesys Token Companion. ʕ•͡ᴥ•ʔ"};
 });
 
+function browse(command, params){
+  var url = 'http://localhost:8003/public/' + command + '.html?account=' + params.account
+  return {
+          title: "Browser",
+          dynamicTitle: true,
+          singleLineInput: true,
+          actions: [ { type: status.actions.FULLSCREEN } ],
+          markup: status.components.bridgedWebView(url)
+  };
+}
+
 status.command({
     name: "leaderboard",
     title: 'SGT token holder leaderboard',
     description: 'Browse SGT token holder leaderboard',
     fullscreen: true,
-    params: [{
-        name: "account",
-        type: status.types.TEXT,
-        placeholder: "SGT holding account address"
-    }],
+    params: [
+      {
+          name: "account",
+          type: status.types.TEXT,
+          placeholder: "SGT holding account address"
+      }
+    ],
     onSend: function(params){
-      var url = 'http://localhost:8003/public/leaderboard.html?account=' + params.account
-      return {
-              title: "Browser",
-              dynamicTitle: true,
-              singleLineInput: true,
-              actions: [ { type: status.actions.FULLSCREEN } ],
-              markup: status.components.bridgedWebView(url)
-      };
+      return browse('leaderboard', params)
     }
 });
