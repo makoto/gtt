217d06d06dc79437042ae6c7cecead779a72105d
Makoto Inoue <inouemak@googlemail.com>
2017-06-05 18:35:30 +0900

    Add leaderboard command

--- bots/welcome.js
+++ bots/welcome.js
@@ -1,3 +1,25 @@
 status.addListener("init", function () {
-  return {"text-message": "Welcome to Genesys Token Companion."};
+  return {"text-message": "Welcome to Genesys Token Companion. ʕ•͡ᴥ•ʔ"};
+});
+
+status.command({
+    name: "leaderboard",
+    title: 'SGT token holder leaderboard',
+    description: 'Browse SGT token holder leaderboard',
+    fullscreen: true,
+    params: [{
+        name: "address",
+        type: status.types.TEXT,
+        placeholder: "SGT holding account address"
+    }],
+    onSend: function(params){
+      var url = 'http://localhost:8003/public/leaderboard.html?account=' + params.account
+      return {
+              title: "Browser",
+              dynamicTitle: true,
+              singleLineInput: true,
+              actions: [ { type: status.actions.FULLSCREEN } ],
+              markup: status.components.bridgedWebView(url)
+      };
+    }
 });
